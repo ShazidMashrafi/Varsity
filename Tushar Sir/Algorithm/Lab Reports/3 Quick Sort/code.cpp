6c5233d55cfb7f8e91e19cc8ce7c257c526006c9
ShazidMahsrafi <shazid456@gmail.com>
2024-09-09 16:28:52 +0600

    updated formatting

--- Tushar Sir/Algorithm/Lab Reports/3 Quick Sort/code.cpp
+++ Tushar Sir/Algorithm/Lab Reports/3 Quick Sort/code.cpp
@@ -6,25 +6,28 @@ int arr[N];
 
 int Partition(int l, int r)
 {
-    int i,j;
-    i=l, j=r;
-    while(i<j)
+    int i, j;
+    i = l, j = r;
+    while (i < j)
     {
-        while(arr[l]>arr[i]) i++;
-        while(arr[l]<arr[j]) j--;
-        if(i<j)  swap(arr[i],arr[j]);
+        while (arr[l] > arr[i])
+            i++;
+        while (arr[l] < arr[j])
+            j--;
+        if (i < j)
+            swap(arr[i], arr[j]);
     }
-    swap(arr[l],arr[j]);
+    swap(arr[l], arr[j]);
     return j;
 }
 
 void quickSort(int l, int r)
 {
-    if(l<r)
+    if (l < r)
     {
-        int m=Partition(l, r);
-        quickSort(l, m-1);
-        quickSort(m+1, r);
+        int m = Partition(l, r);
+        quickSort(l, m - 1);
+        quickSort(m + 1, r);
     }
 }
 
@@ -32,7 +35,8 @@ int main()
 {
     int n;
     cin >> n;
-    for (int i = 0; i <n; ++i) cin >> arr[i];
+    for (int i = 0; i < n; ++i)
+        cin >> arr[i];
     arr[n] = INT_MAX;
     quickSort(0, n);
     for (int i = 0; i < n; ++i)
