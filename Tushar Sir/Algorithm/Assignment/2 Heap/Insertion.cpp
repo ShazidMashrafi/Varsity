16c76ec57ca25319ba9d98be23dd39446faf6616
ShazidMahsrafi <shazid456@gmail.com>
2024-10-17 02:37:47 +0600

    ok

--- Tushar Sir/Algorithm/Assignment/2 Heap/Insertion.cpp
+++ Tushar Sir/Algorithm/Assignment/2 Heap/Insertion.cpp
@@ -1,26 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1e5+10;
+const int N = 1e5 + 10;
 int a[N];
 
 void display(int n)
 {
-    //Displays the current heap
-    for(int i=1; i<=n; ++i)
-        cout<<a[i]<<" ";
-    cout<<endl;
+    // Displays the current heap
+    for (int i = 1; i <= n; ++i)
+        cout << a[i] << " ";
+    cout << endl;
 }
 
 int insert(int n)
 {
     // Inserts a[n] into the heap which is stored in a[1:n-1]
-    int i=n;
+    int i = n;
     int item = a[n];
-    while(i>1 && a[i/2]<item)
+    while (i > 1 && a[i / 2] < item)
     {
-        a[i] = a[i/2];
-        i = i/2;
+        a[i] = a[i / 2];
+        i = i / 2;
     }
     a[i] = item;
     return true;
@@ -28,14 +28,14 @@ int insert(int n)
 
 int main()
 {
-    int n=0;
+    int n = 0;
     int ele;
-    while(cin>>ele)
+    while (cin >> ele)
     {
-        cout<<"Inserting "<<ele<<" into heap"<<endl;
+        cout << "Inserting " << ele << " into heap" << endl;
         n++;
-        a[n]=ele;
-        cout<<"After insertion: "<<ele<<" : ";
+        a[n] = ele;
+        cout << "After insertion: " << ele << " : ";
         display(n);
     }
 }
